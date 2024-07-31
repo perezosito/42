469abc9367440209f7a57c6c23b17e522b73c8a5
Mikel Perez Gomez <miperez-@c2r6s2.42urduliz.com>
2024-07-31 13:48:50 +0200

    actualizacion BSQ

--- BSQ/main.c
+++ BSQ/main.c
@@ -14,102 +14,195 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-// Estructura para almacenar la información del mapa
-typedef struct {
-    int num_lines;
-    char empty_char;
-    char obstacle_char;
-    char full_char;
-    char **map;
-} t_map;
+typedef struct
+{
+	int	num_lines;
 
-// Función para leer la primera línea y obtener la información del mapa
-t_map *read_map_info(int fd) {
-    char buffer[1024];
-    int i = 0;
-    char c;
+	char	empty_char;
+	char	obstacle_char;
+	char	full_char;
+	char	**map;
+}
+t_map;
 
-    // Leer la primera línea
-    while (read(fd, &c, 1) > 0 && c != '\n' && i < 1023) {
-        buffer[i++] = c;
-    }
-    buffer[i] = '\0';
+// Leer la información del mapa
+t_map	*read_map_info(int fd)
+{
+	char	buffer[1024];
+	int	i = 0;
+	char	c;
 
-    // Crear estructura para el mapa
-    t_map *map_info = (t_map *)malloc(sizeof(t_map));
-    if (!map_info) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
+	while (read(fd, &c, 1) > 0 && c != '\n' && i < 1023)
+	{
+		buffer[i++] = c;
+	}
+	buffer[i] = '\0';
 
-    // Analizar la primera línea
-    map_info->num_lines = atoi(buffer); // Número de líneas del mapa
-    int len = i;
-    map_info->empty_char = buffer[len - 3]; // Carácter vacío
-    map_info->obstacle_char = buffer[len - 2]; // Carácter obstáculo
-    map_info->full_char = buffer[len - 1]; // Carácter lleno
+	t_map *map_info = (t_map *)malloc(sizeof(t_map));
+	if (!map_info)
+	{
+        	perror("malloc");
+        	exit(EXIT_FAILURE);
+	}
 
-    return map_info;
+	map_info->num_lines = atoi(buffer);
+	int len = i;
+	map_info->empty_char = buffer[len - 3];
+	map_info->obstacle_char = buffer[len - 2];
+	map_info->full_char = buffer[len - 1];
+
+	return map_info;
 }
 
-// Función para leer el contenido del mapa
-void read_map_content(int fd, t_map *map_info) {
-    map_info->map = (char **)malloc(sizeof(char *) * map_info->num_lines);
-    if (!map_info->map) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
+// Leer el contenido del mapa
+void read_map_content(int fd, t_map *map_info)
+{
+	int i = 0;
+	char c;
 
-    char c;
-    for (int i = 0; i < map_info->num_lines; i++) {
-        int j = 0;
-        map_info->map[i] = (char *)malloc(sizeof(char) * 1024);
-        if (!map_info->map[i]) {
-            perror("malloc");
-            exit(EXIT_FAILURE);
-        }
+	map_info->map = (char **)malloc(sizeof(char *) * map_info->num_lines);
+	if (!map_info->map)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
 
-        while (read(fd, &c, 1) > 0 && c != '\n') {
-            map_info->map[i][j++] = c;
-        }
-        map_info->map[i][j] = '\0';
-    }
+	while (i < map_info->num_lines)
+	{
+		int j = 0;
+		map_info->map[i] = (char *)malloc(sizeof(char) * 1024);
+        	if (!map_info->map[i])
+		{
+			perror("malloc");
+			exit(EXIT_FAILURE);
+		}
+
+        while (read(fd, &c, 1) > 0 && c != '\n')
+	{
+		map_info->map[i][j++] = c;
+	}
+	map_info->map[i][j] = '\0';
+	i++;
+	}
 }
 
-int main(int argc, char **argv) {
-    if (argc < 2) {
-        write(2, "Usage: ./bsq <filename>\n", 25);
-        return 1;
-    }
+// Encontrar el mayor cuadrado en el mapa
+void	find_largest_square(t_map *map_info)
+{
+	int **aux;
+	int max_size = 0;
+	int max_i = 0;
+	int max_j = 0;
+	int i = 0;
 
-    int fd = open(argv[1], O_RDONLY);
-    if (fd < 0) {
-        perror("open");
-        return 1;
-    }
+	aux = (int **)malloc(sizeof(int *) * map_info->num_lines);
+	while (i < map_info->num_lines)
+	{
+		aux[i] = (int *)malloc(sizeof(int) * 1024);
+		i++;
+	}
 
-    // Leer la información del mapa
-    t_map *map_info = read_map_info(fd);
+	i = 0;
+	while (i < map_info->num_lines)
+	{
+		int j = 0;
+		while (map_info->map[i][j] != '\0')
+		{
+			if (map_info->map[i][j] == map_info->obstacle_char)
+			{
+			aux[i][j] = 0;
+			}
+			else if (i == 0 || j == 0)
+			{
+				aux[i][j] = 1;
+			}
+			else
+			{
+				int min = aux[i - 1][j];
+				if (aux[i][j - 1] < min)
+				{
+					min = aux[i][j - 1];
+				}
+				if (aux[i - 1][j - 1] < min)
+				{
+					min = aux[i - 1][j - 1];
+				}
+				aux[i][j] = 1 + min;
+			}
 
-    // Leer el contenido del mapa
-    read_map_content(fd, map_info);
+			if (aux[i][j] > max_size)
+			{
+				max_size = aux[i][j];
+				max_i = i;
+				max_j = j;
+			}
+			j++;
+		}
+		i++;
+	}
 
-    // Cerrar el archivo
-    close(fd);
+	i = max_i;
+	while (i > max_i - max_size)
+	{
+		int j = max_j;
+		while (j > max_j - max_size)
+		{
+			map_info->map[i][j] = map_info->full_char;
+			j--;
+		}
+		i--;
+	}
 
-    // Imprimir el mapa leído (solo para verificar)
-    for (int i = 0; i < map_info->num_lines; i++) {
-        write(1, map_info->map[i], 1024);
-        write(1, "\n", 1);
-    }
-
-    // Liberar memoria
-    for (int i = 0; i < map_info->num_lines; i++) {
-        free(map_info->map[i]);
-    }
-    free(map_info->map);
-    free(map_info);
-
-    return 0;
+	i = 0;
+	while (i < map_info->num_lines)
+	{
+	free(aux[i]);
+	i++;
+	}
+	free(aux);
 }
 
+int	main(int argc, char **argv)
+{
+	int fd;
+	t_map *map_info;
+	int i;
+
+	if (argc < 2)
+	{
+		write(2, "Usage: ./bsq <filename>\n", 25);
+		return 1;
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		perror("open");
+		return 1;
+	}
+
+	map_info = read_map_info(fd);
+	read_map_content(fd, map_info);
+	close(fd);
+
+	find_largest_square(map_info);
+
+	i = 0;
+	while (i < map_info->num_lines)
+	{
+	write(1, map_info->map[i], 1024);
+	write(1, "\n", 1);
+	i++;
+	}
+
+	i = 0;
+	while (i < map_info->num_lines)
+	{
+		free(map_info->map[i]);
+		i++;
+	}
+	free(map_info->map);
+	free(map_info);
+
+	return 0;
+}
